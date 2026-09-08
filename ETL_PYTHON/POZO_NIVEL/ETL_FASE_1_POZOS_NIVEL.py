597a8ebb5d4d7afed2d9fa9f3899ddfcebb5fb49
DerinsonRojas <derinsonrojas@gmail.com>
2026-09-08 18:11:37 +0200

    feat(pozos_nivel): inyección inicial de datos crudos en PostgreSQL para fase de limpieza

--- ETL_PYTHON/POZO_NIVEL/ETL_FASE_1_POZOS_NIVEL.py
+++ ETL_PYTHON/POZO_NIVEL/ETL_FASE_1_POZOS_NIVEL.py
@@ -1,12 +1,11 @@
 """
-MÓDULO: Pipeline de Extracción, Limpieza y Carga (ETL) - Datos de Pozos Litoología
-PROYECTO: SIGATLAS - Tesis de Grado
+MÓDULO: Pipeline de Extracción, Limpieza y Carga (ETL) - Datos de Quimica de Pozos
+PROYECTO: Prueba de Concepto - Tesis de Grado
 AUTOR: DERINSON ROJAS
 DESCRIPCIÓN: 
-    ETL simplificado para pozos_litologia. 
-    Las reglas de negocio (normalización, validaciones, reglas telescópicas, 
-    booleanos, estatus, etc.) ahora se ejecutan en PostgreSQL mediante 
-    función + trigger. Este script solo limpia datos crudos y carga.
+    (Script de ETL y conección a BBDD PostgresSQL funcional) 
+    Este script automatiza la ingesta y aplica las reglas de validación técnica
+    identificadas en la auditoría de datos para los datos de nivel de pozos.
 """
 
 import pandas as pd
@@ -19,11 +18,11 @@ import os
 # ==========================================
 print(">> Iniciando extracción de datos...")
 df = pd.read_csv(
-    "pozosLitologia.txt", 
+    "POZOS NIVEL.txt", 
     sep=";", 
     encoding="Latin-1",
-    na_values=["", " "],
-    skipinitialspace=True
+    na_values=["", " "],       
+    skipinitialspace=True      
 )
 
 # ==========================================
@@ -36,25 +35,21 @@ df.columns = df.columns.str.lower()
 
 diccionario_nombres = {
     'iden': 'id_pozo',
-    'lito': 'litologia',
-    'act_litolo':'act_litologia'
-    
+    'ano': 'anio',   
 }
+
 df = df.rename(columns=diccionario_nombres)
 
 # 2.2. Conversión de columnas numéricas
 columnas_numericas = [
-    'desde', 'hasta'
+    'm1', 'm2','m3','m4','m5','m6','m7','m8','m9','m10','m11','m12'
 ]
-
 for col in columnas_numericas:
     if col in df.columns:
-        df[col] = df[col].astype(str).str.replace(',', '.')
         df[col] = pd.to_numeric(df[col], errors='coerce')
 
-
 # 2.3. Estandarización de fechas (solo limpieza básica)
-df['act_litologia'] = pd.to_datetime(df['act_litologia'], dayfirst=True, errors='coerce').dt.normalize()
+df['act_nivel'] = pd.to_datetime(df['act_nivel'], dayfirst=True, errors='coerce').dt.normalize()
 
 # ==========================================
 # 3. EXPORTACIÓN A CSV (DATOS_PROCESADOS)
@@ -63,7 +58,7 @@ ruta_salida = os.path.join(
     os.path.dirname(__file__),
     "..", "..",
     "DATOS_PROCESADOS",
-    "DATOS_POST_ETL_POZOS_LITOLOGIA.csv"
+    "DATOS_POST_ETL_POZOS_NIVEL.csv"
 )
 
 print(">> Exportando archivo CSV procesado...")
@@ -86,16 +81,16 @@ engine = create_engine(URL_CONEXION)
 
 # 4.1. Vaciar tabla pero mantener estructura y vistas
 with engine.begin() as con:
-    print(">> TRUNCATE TABLE pozos_litologia (manteniendo estructura y vistas)...")
-    con.execute(text("TRUNCATE TABLE public.pozos_litologia;"))
+    print(">> TRUNCATE TABLE pozos_nivel (manteniendo estructura y vistas)...")
+    con.execute(text("TRUNCATE TABLE public.pozos_nivel;"))
 
 # 4.2. Carga de datos (el trigger aplica reglas de negocio)
-print(">> Inyectando datos en pozos_litologia...")
+print(">> Inyectando datos en pozos_nivel...")
 df.to_sql(
-    name="pozos_litologia",
+    name="pozos_nivel",
     con=engine,
     if_exists="append",
     index=False
 )
 
-print(">> ETL pozos_litologia ejecutado con éxito.")
+print(">> ETL pozos_nivel ejecutado con éxito.")
